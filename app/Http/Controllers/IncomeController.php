42d026cbcb08b12f503dc6cf72e202e9a8fa2b30
babylion <btarialiya93@gmail.com>
2020-10-30 08:10:35 +0700

    income feature updated

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -13,8 +13,9 @@ class IncomeController extends Controller
      */
     public function index()
     {
-        $data_order = \App\Models\Order::select('id_order')->whereIn('status', ['payment received', 'order sent', 'order processed'])->get();
-        $data_payment = \App\Models\Payment::whereIn('fk_id_order', $data_order)->sum('total_payment');
+        $data_order = \App\Models\Order::select('id_order', 'status')->whereIn('status', ['payment received', 'order sent', 'order processed'])->get();
+        $id_order = \App\Models\Order::select('id_order')->whereIn('status', ['payment received', 'order sent', 'order processed'])->get();
+        $data_payment = \App\Models\Payment::whereIn('fk_id_order', $id_order)->sum('total_payment');
 
         return view('income.index', ['data_order' => $data_order, 'data_payment' => $data_payment]);
     }
--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -3,13 +3,25 @@
 @section('Income', 'active')
 
 @section('content')
-    <div class="card text-center mr-auto ml-auto mt-5" style="width: 18rem;" >
-        <div class="card-header">
-            Income
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Da Venti's Income</h5>
-            <p class="card-text">Rp{{$data_payment}}</p>
-        </div>
-    </div>
+    <h1>Da Venti's Income</h1>
+    <table class="table table-hover table-bordered">
+        <thead class="thead-dark">
+        <tr>
+            <th>ID Order</th>
+            <th>Status</th>
+            <th>Total Payment</th>
+        </tr>
+        </thead>
+        @foreach($data_order as $order)
+        <tr>
+            <td>{{$order->id_order}}</td>
+            <td>{{$order->status}}</td>
+            <td>Rp{{$order->payment->total_payment}}</td>
+        </tr>
+        @endforeach
+        <tr>
+            <td colspan="2" class="table-dark font-weight-bold">Total Income</td>
+            <td class="table-success text-bold font-weight-bold">Rp{{$data_payment}}</td>
+        </tr>
+    </table>
 @endsection
